e2a6e087628d1e1d1a73750a7e68f61fa35a0427
kashsid <kashifsiddique@gmail.com>
2019-06-11 09:04:52 -0500

    implements basic layout for calculator

--- src/App.js
+++ src/App.js
@@ -7,6 +7,38 @@ class App extends Component{
   return (
     <div className="App">
       <h1>Calculator</h1>
+      <div className="calculator-wrapper">
+        <div className='calc-row'>
+          <input></input>
+        </div>
+        <div className='calc-row'>
+          <button>7</button>
+          <button>8</button>
+          <button>9</button>
+          <button>/</button>
+        </div>
+        <div className='calc-row'>
+          <button>4</button>
+          <button>5</button>
+          <button>6</button>
+          <button>*</button>
+        </div>
+        <div className='calc-row'>
+          <button>1</button>
+          <button>2</button>
+          <button>3</button>
+          <button>+</button>
+        </div>
+        <div className='calc-row'>
+          <button>.</button>
+          <button>0</button>
+          <button>=</button>
+          <button>-</button>
+        </div>
+        <div className='calc-row'>
+          <button>Clear</button>
+        </div>
+      </div>
     </div>
   );
 }
